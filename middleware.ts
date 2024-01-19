5d878142ac806a88e6a24a3c3ab42f8bd07461da
zakaWAC <129171507+zakaWAC@users.noreply.github.com>
2024-01-19 18:53:08 +0100

    Add Webhook_Secret

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
     publicRoutes: [
-        "/"
+        "/",
+        "/api/webhook",
+    ],
+    ignoredRoutes: [
+      '/api/webhook'
     ]
 });
  
